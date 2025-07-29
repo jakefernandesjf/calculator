821c7aa475f7fb78c80eb97a3699e8e5a350ad25
jakefernandesjf <46454692+jakefernandesjf@users.noreply.github.com>
2025-07-29 15:03:01 -0700

    Create operate function and operation variables

--- javascript.js
+++ javascript.js
@@ -1,5 +1,24 @@
 // javascript.js
 
+let userFirstNumber;
+let userOperator;
+let userSecondNumber;
+
+
+function operate(operator, firstNumber, secondNumber) {
+    switch (operator) {
+        case ("+"):
+            return add(firstNumber, secondNumber);
+        case ("-"):
+            return subtract(firstNumber, secondNumber);
+        case ("*"):
+            return multiply(firstNumber, secondNumber);
+        case ("/"):
+            return divide(firstNumber, secondNumber);
+    }
+}
+
+
 function add(a, b) {
     return a + b;
 }
